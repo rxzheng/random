5ba2e62ab4c4b8d556e6ab8ad4551afb2971bdb9
rxzheng <richardxzheng@outlook.com>
2024-04-02 22:50:52 +1100

    made a safe function :)

---
+++ safefunction.dart
@@ -0,0 +1,11 @@
+void main() {
+  print('hui guiss!!');
+  whatTheFlip(name: 'DAVE', gender: 'id prefer not to say', age: 5);
+}
+
+void whatTheFlip(
+    {required String name, required String gender, required int age}) {
+  print(name);
+  print(gender);
+  print(age);
+}
